65abf02bb4d4ca000da18dc31c2c8662a39a38e9
Cătălin Mariș <alrraa@gmail.com>
2017-05-27 12:13:10 +0300

    Deprecate project 🎉
    Travis CI now supports this functionality via `Build Stages`:
    https://blog.travis-ci.com/2017-05-11-introducing-build-stages
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix #23
    
    Close #6
    Close #13
    Close #15
    Close #18

--- test/utils/sha256.js
+++
@@ -1,6 +0,0 @@
-import crypto from 'crypto';
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-export default (text, key) =>
-    crypto.createHmac('sha256', key).update(`${text}`).digest('hex');
